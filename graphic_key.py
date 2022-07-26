561e929aad6bff79fb9399279d7776702cd094a0
ikvict07 <antongorobec101@gmail.com>
2022-07-26 23:17:05 +0300

    26,07,22

--- graphic_key.py
+++ graphic_key.py
@@ -1,96 +1,86 @@
+def count_patterns_from(first_point: str, length: int, seq=None) -> int:
+    """
+Функция, чтобы посчитать количество возможных паролей графического ключа, длины length, если начинать с точки
+firstPoint Первый аргумент - начальная точка, второй - длина пароля, остальные не заполнять.
 
+вид граф ключа:
+[A] [B] [C]
+[D] [E] [F]
+[G] [H] [I]
+"""
 
-def count_patterns_from(firstPoint: str, length: int, firs_call = True, seq = None) -> int:
-	'''
-	Функция чтобы посчитать количество возможных паролей графического ключа, длины length, если начинать с точки firstPoint
-	Первый аргумент - начальная точка, второй - длина пароля, остальные не заполнять.
+    counter = 0  # То, что мы будем возвращать (количество паролей)
 
-	вид граф ключа:
-	[A] [B] [C]
-	[D] [E] [F]
-	[G] [H] [I]
+    if seq is None:  # Если это первый вызов, то создаём список, в который потом будем записывать составляющие ключа
+        # (буквы)
+        seq = []
 
+    current_length = length
 
-	'''
+    current_point = first_point
 
+    seq.append(current_point)
 
-	counter = 0   # То, что мы будем возвращать (количество паролей)
+    # Крайний случай, и случай некорректных данных
 
-	if seq is None: # Если это первый вызов, то создаём список, в который потом будем записывать составляющие ключа (буквы)
-		seq = []
+    if length > 9 or length < 1:
+        return 0
+    if length == 1:
+        return 1
 
-	
+    # Списки букв, до которых можно дотянуться с определённой точки
 
-	curent_length = length
+    A = ['B', 'E', 'D', 'H', 'F']
+    B = ['A', 'C', 'D', 'E', 'F', 'G', 'I']
+    C = ['B', 'E', 'F', 'D', 'H']
+    D = ['A', 'B', 'C', 'E', 'H', 'I', 'G']
+    E = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I']
+    F = ['A', 'B', 'C', 'E', 'G', 'H', 'I']
+    G = ['D', 'E', 'H', 'F', 'B']
+    H = ['G', 'D', 'A', 'E', 'C', 'F', 'I']
+    I = ['H', 'D', 'E', 'B', 'F']
 
-	curent_point = firstPoint
+    my_di = {'A': A, 'B': B, 'C': C,
+             'D': D, 'E': E, 'F': F,
+             'G': G, 'H': H, 'I': I}
 
-	seq.append(curent_point)
+    # Если Е участвует в ключе, то список букв, к которым можно дотянуться с определённой буквы, будет больше
 
-	# Крайний случай, и случай некоректных данных
+    if 'E' in seq:
+        A.append('I')
+        B.append('H')
+        C.append('G')
+        D.append('F')
+        F.append('D')
+        G.append('C')
+        H.append('B')
+        I.append('A')
 
-	if length > 9 or length < 1:
-		return 0
-	if length == 1:
-		return 1
+    # Буквы не могут дотянуться друг до друга, если между ними есть другая буква, но если эта буква уже зажата,
+    # то это возможно
+    if 'D' in seq:
+        A.append('G')
+        G.append('A')
+    if 'B' in seq:
+        A.append('C')
+        C.append('A')
+    if 'F' in seq:
+        C.append('I')
+        I.append('C')
+    if 'H' in seq:
+        I.append('G')
+        G.append('I')
 
-	# Списки букв, до которых можно дотянуться с определённой точки
+    # Перебор всех возможных ключей нужной длины
 
-	A = ['B', 'E', 'D', 'H', 'F']
-	B = ['A', 'C', 'D', 'E', 'F', 'G', 'I']
-	C = ['B', 'E', 'F', 'D', 'H']
-	D = ['A', 'B', 'C', 'E', 'H', 'I', 'G']
-	E = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I']
-	F = ['A', 'B', 'C', 'E', 'G', 'H', 'I']
-	G = ['D', 'E', 'H', 'F', 'B']
-	H = ['G', 'D', 'A', 'E', 'C', 'F', 'I']
-	I = ['H', 'D', 'E', 'B', 'F']
+    for point in my_di[current_point]:
 
-	lst_of_all = [A, B, C, D, E, F, G, H, I]
+        if point not in seq:
+            counter += count_patterns_from(point, current_length - 1, seq=seq)
 
-	my_di = {'A': A, 'B': B, 'C': C,
-	         'D': D, 'E': E, 'F': F,
-	         'G': G, 'H': H, 'I': I}
+            seq.pop()
 
-
-	# Если Е участвует в ключе, то список букв, к которым можно дотянуться с определённой буквы, будет больше
-
-	if 'E' in seq:
-		A.append('I')
-		B.append('H')
-		C.append('G')
-		D.append('F')
-		F.append('D')
-		G.append('C')
-		H.append('B')
-		I.append('A')
-
-	# Буквы не могут дотянуться друг до друга, если между ними есть другая буква, но если эта буква уже зажата, то это возможно
-	if 'D' in seq:
-		A.append('G')
-		G.append('A')
-	if 'B' in seq:
-		A.append('C')
-		C.append('A')
-	if 'F' in seq:
-		C.append('I')
-		I.append('C')
-	if 'H' in seq:
-		I.append('G')
-		G.append('I')
-
-
-	# Перебор всех возможных ключей нужной длины
-
-	for point in my_di[curent_point]:
-
-		if point not in seq:
-
-			counter += count_patterns_from(point, curent_length-1, firs_call = False, seq = seq)
-			
-			seq.pop()
-		
-	return counter
+    return counter
 
 
 # print(count_patterns_from('B',1))
@@ -106,17 +96,17 @@ def count_patterns_from(firstPoint: str, length: int, firs_call = True, seq = No
 # print(count_patterns_from('E',4)) 
 
 
-def total_posible_combs():
-	"""
-	Сколько есть всего комбинаций для графического ключа телефона (обычно пароль состоит из минимум 4 символов и максимум 9) 
-	(
-	"""
-	count = 0
+def total_possible_combs():
+    """
+Сколько есть всего комбинаций для графического ключа телефона (обычно пароль состоит минимум из 4 символов и максимум 9)
+"""
+    count = 0
 
-	for i in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']:
-		for j in range(4,10):
-			count += count_patterns_from(i, j)
+    for i in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']:
+        for j in range(4, 10):
+            count += count_patterns_from(i, j)
 
-	print(count)
+    print(count)
 
-total_posible_combs()
+
+total_possible_combs()
